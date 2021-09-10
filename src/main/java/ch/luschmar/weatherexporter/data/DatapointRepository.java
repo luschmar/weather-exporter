e6fd2d189bff3b03d38c94df4417037cdb66a2b2
dmfxw2h704o <dmfxw2h704o@temp.mailbox.org>
2021-09-10 23:54:55 +0200

    remove jpa; h2 database

--- src/main/java/ch/luschmar/weatherexporter/data/DatapointRepository.java
+++
@@ -1,7 +0,0 @@
-package ch.luschmar.weatherexporter.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DatapointRepository extends JpaRepository<Datapoint, String>{
-
-}
